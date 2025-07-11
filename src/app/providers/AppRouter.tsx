889d5a7d2a0cf6c7bf3e0aeaf4969e79166238af
Egor <tumkasuv811@gmail.com>
2025-07-11 18:00:58 +0500

    upd2

--- src/app/providers/AppRouter.tsx
+++ src/app/providers/AppRouter.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import HomePage from '../../pages/HomePage/HomePage';
 import Calculations from '../../pages/Calculations/Calculations';
